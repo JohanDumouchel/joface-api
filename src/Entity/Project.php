3e755d0c35926ea71a6d7e6074de72427b1e6c25
Johan <johandumouchel@gmail.com>
2020-03-20 15:43:21 +0100

    Create SkillCategory Entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
+ */
+class Project
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=1000, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateStart;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateEnd;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $SkillCategory;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateStart(): ?\DateTimeInterface
+    {
+        return $this->dateStart;
+    }
+
+    public function setDateStart(\DateTimeInterface $dateStart): self
+    {
+        $this->dateStart = $dateStart;
+
+        return $this;
+    }
+
+    public function getDateEnd(): ?\DateTimeInterface
+    {
+        return $this->dateEnd;
+    }
+
+    public function setDateEnd(?\DateTimeInterface $dateEnd): self
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+
+    public function getSkillCategory(): ?string
+    {
+        return $this->SkillCategory;
+    }
+
+    public function setSkillCategory(string $SkillCategory): self
+    {
+        $this->SkillCategory = $SkillCategory;
+
+        return $this;
+    }
+}
